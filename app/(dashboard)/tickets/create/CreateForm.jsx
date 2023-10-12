07f27313facd258b1636cfd917dd5f319a68a5a4
Marco Aurélio Prado <marco.pdsv@gmail.com>
2023-10-12 12:31:22 -0300

    ...

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -2,69 +2,34 @@
 
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { addTicket } from '../actions';
 
 export default function CreateForm() {
   const router = useRouter()
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-  const [priority, setPriority] = useState('low')
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setIsLoading(true)
-
-    const ticket = {
-      title,
-      body,
-      priority,
-    }
-
-    const response = await fetch('http://localhost:3000/api/tickets', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(ticket)
-    })
-
-    const json = await response.json()
-
-    if (json.error) {
-      console.log(json.error)
-    }
-
-    if (json.data) {
-      router.refresh()
-      router.push('/tickets')
-    }
-
-    setIsLoading(false)
-  }
-
   return (
-    <form onSubmit={handleSubmit} className="w-1/2">
+    <form action={addTicket} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
+          name='title'
           required
           type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
         />
       </label>
       <label>
         <span>Body:</span>
         <textarea
+          name='body'
           required
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
         />
       </label>
       <label>
         <span>Priority:</span>
         <select
-          onChange={(e) => setPriority(e.target.value)}
-          value={priority}
+          name='priority'
         >
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
